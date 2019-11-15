7b63e014f007b03a533e1b463fbc5e91a819bc67
Simone Anthony <simoneanthony@Simones-MBP.attlocal.net>
2019-11-15 12:29:23 -0500

    plenty oof major edits

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,15 +3,24 @@ import { Jumbotron, Container } from 'reactstrap';
 
 const HomePage = () => {
 
+    const mapBlog = () => {
+    return <div>testing</div>
+    }
     return (
-        <div>
-            <div>
+        <div className="homepage-container">
+            <div className="homepage-hero vh-100">
                 <Jumbotron fluid>
-                    <Container fluid>
-                        <h1 className="display-3">Fluid jumbotron</h1>
-                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    <Container className="hero-text-container">
+                        <h1>Data Structure<br/> and Algorithm</h1>
+                        <p>As javascript becomes more of a software development language, I find it increasingly important to document common software development problems in the language its self.
+                            In laymans terms, the purpose of this blog is to provide javascript solutions for traditional D.S. and A. questions that would typically be written in Java or C++. I am also writing this blog for the added of bonus practicing these solutions myself (and to become more appealing to potential employers <span role="img" aria-label="smirk-emoji">&#128521;</span>). 
+                            I hope this can be of use to both myself and others in search of both knowledge and skill in regards to software-development based javascript.
+                        </p>
                     </Container>
                 </Jumbotron>
+            </div>
+            <div className="homepage-body vh-100">
+                {mapBlog()}
             </div>
         </div>);
 
---
+++ src/js/all-blocks.js
@@ -0,0 +1,27 @@
+const homePageBlogs = [
+    {
+        id: 1,
+        DSOrA:'Algorithm',
+        link: '#',
+        Header: 'Reverse a String',
+        description: 'Take the string and reverse it using a decrementing for loop.',
+
+    },
+    {
+        id: 2,
+        DSOrA:'Algorithm',
+        link: '#',
+        Header: 'Palindrome',
+        description: 'Take the string, reverse it, and compare it to the original input.',
+
+    },{
+        id: 3,
+        DSOrA:'Algorithm',
+        link: '#',
+        Header: 'Integer Reversal',
+        description: 'Take an integer and reverse it. ',
+
+    }
+]
+
+export default homePageBlogs;
