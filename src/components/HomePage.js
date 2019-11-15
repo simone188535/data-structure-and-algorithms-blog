a8ce49c45c72fc4d5c6707089febb72c75d0fa58
Simone Anthony <simoneanthony@Simones-MBP.attlocal.net>
2019-11-15 00:06:34 -0500

    sass added and other edits

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
 
-const HomePage = () =>{
+const HomePage = () => {
 
-        return <div>Hello Home-Page</div>;
-    
+    return (
+        <div>
+            <div>
+                <Jumbotron fluid>
+                    <Container fluid>
+                        <h1 className="display-3">Fluid jumbotron</h1>
+                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    </Container>
+                </Jumbotron>
+            </div>
+        </div>);
+
 }
 
 export default HomePage;
