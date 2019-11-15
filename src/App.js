721d97c30cc9f9138995b9b92f6a8fb60490fde0
Simone Anthony <simoneanthony@Simones-MBP.attlocal.net>
2019-11-14 22:45:21 -0500

    some files removed. others created. file structures inital organization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+
+
+const App = () => {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+
+}
+
+
+export default App;
